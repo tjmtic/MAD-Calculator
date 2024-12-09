9590d720bc893dc1dfe9829c2a8fa25556a3535a
TM <tjmcard@gmail.com>
2024-12-08 21:18:25 -0800

    Project Settings Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.snapapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.snapapp"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "My Application"
+rootProject.name = "Snap App"
 include(":app")
  
